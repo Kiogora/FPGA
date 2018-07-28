d7f81c225899ae7c81148247d5d914dd2d7fc97c
Alois Mbutura <aloismbutura@gmail.com>
2018-07-28 15:08:21 +0300

    Write initial source file version for the esp32

--- Basys_3/fpga_spi_slave_simple/main/main.c
+++ Basys_3/fpga_spi_slave_simple/main/main.c
@@ -1,202 +1,97 @@
+/*
+The spi transaction is as follows => command | address | data
+For our purposes, we always do a full duplex Write and read. We write the next LED state and read the current LED state.
+There is no command or address bits in the transaction, which contains data only.
+*/ 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "esp_spi_flash.h"
-#include "freertos/event_groups.h"
-#include "esp_wifi.h"
+#include "driver/spi_master.h"
+#include "esp_err.h"
 #include "esp_log.h"
-#include "esp_event_loop.h"
-#include "nvs_flash.h"
-#include "lwip/inet.h"
-#include "lwip/ip4_addr.h"
-#include "lwip/dns.h"
-#include "ping.h"
-#include "esp_ping.h"
 
+#define PIN_NUM_MISO 25
+#define PIN_NUM_MOSI 23
+#define PIN_NUM_CLK  19
+#define PIN_NUM_CS   22
 
-/*Set the SSID and Password via "make menuconfig"*/
-#define DEFAULT_SSID "EED Sky"
-#define DEFAULT_PWD "20150815"
+#define SCLK_SPEED 10000000UL
 
-#define TRUE 1
-#define FALSE 0
+static const char* TAG = "[FPGA_SPI_SLAVE]";
 
-#if CONFIG_WIFI_ALL_CHANNEL_SCAN
-#define DEFAULT_SCAN_METHOD WIFI_ALL_CHANNEL_SCAN
-#elif CONFIG_WIFI_FAST_SCAN
-#define DEFAULT_SCAN_METHOD WIFI_FAST_SCAN
-#else
-#define DEFAULT_SCAN_METHOD WIFI_FAST_SCAN
-#endif /*CONFIG_SCAN_METHOD*/
-
-#if CONFIG_WIFI_CONNECT_AP_BY_SIGNAL
-#define DEFAULT_SORT_METHOD WIFI_CONNECT_AP_BY_SIGNAL
-#elif CONFIG_WIFI_CONNECT_AP_BY_SECURITY
-#define DEFAULT_SORT_METHOD WIFI_CONNECT_AP_BY_SECURITY
-#else
-#define DEFAULT_SORT_METHOD WIFI_CONNECT_AP_BY_SIGNAL
-#endif /*CONFIG_SORT_METHOD*/
-
-#if CONFIG_FAST_SCAN_THRESHOLD
-#define DEFAULT_RSSI CONFIG_FAST_SCAN_MINIMUM_SIGNAL
-#if CONFIG_EXAMPLE_OPEN
-#define DEFAULT_AUTHMODE WIFI_AUTH_OPEN
-#elif CONFIG_EXAMPLE_WEP
-#define DEFAULT_AUTHMODE WIFI_AUTH_WEP
-#elif CONFIG_EXAMPLE_WPA
-#define DEFAULT_AUTHMODE WIFI_AUTH_WPA_PSK
-#elif CONFIG_EXAMPLE_WPA2
-#define DEFAULT_AUTHMODE WIFI_AUTH_WPA2_PSK
-#else
-#define DEFAULT_AUTHMODE WIFI_AUTH_OPEN
-#endif
-#else
-#define DEFAULT_RSSI -127
-#define DEFAULT_AUTHMODE WIFI_AUTH_OPEN //WIFI_AUTH_WPA2_PSK
-#endif /*CONFIG_FAST_SCAN_THRESHOLD*/
-
-
-char* addr="8.8.8.8";
-
-static const char *TAG = "scan";
-int gotConn = FALSE;
-int first = TRUE;
-
-ip_addr_t ip_Addr;
-
-ip4_addr_t ip;
-ip4_addr_t device;
-ip4_addr_t msk;
-uint32_t ping_count = 25;  //how many pings per report
-uint32_t ping_timeout = 2; //mS till we consider it timed out
-uint32_t ping_delay = 3; //mS between pings
-uint32_t waiting_results = 0;
-
-static esp_err_t event_handler(void *ctx, system_event_t *event)
+void set_read_basys3_leds(spi_transaction_t* transaction, spi_device_handle_t* handler)
 {
-    switch (event->event_id) {
-        case SYSTEM_EVENT_STA_START:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
-            ESP_ERROR_CHECK(esp_wifi_connect());
-            break;
-        case SYSTEM_EVENT_STA_GOT_IP:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
-            ip = event->event_info.got_ip.ip_info.ip;
-            inet_aton(addr, &device);//gw = event->event_info.got_ip.ip_info.gw;
-            msk = event->event_info.got_ip.ip_info.netmask;
-            ESP_LOGI(TAG, "Got IP: %s\n",
-                     ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
-            gotConn=TRUE;
-           
-            break;
-        case SYSTEM_EVENT_STA_DISCONNECTED:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
-            gotConn=FALSE;
-            ping_deinit();
-            ESP_ERROR_CHECK(esp_wifi_connect());
-            break;
-        default:
-            break;
+    //Generate pseudorandom 16 bit value with initial default seed = 1 for traceability during reruns.
+    //The rand() functions keeps current state between calls
+    uint16_t ledstate=rand()%65536;
+
+    //Default initial state is one in order to differentiate it from actual value 0 read if ok during testing
+    uint32_t readstate=1;
+
+    //Stuff 16 bit data to transmit into tx_data array member in transaction struct.
+    transaction->tx_data[0]=(uint8_t)(ledstate & 0x00ff);
+    transaction->tx_data[1]=(uint8_t)(ledstate & 0xff00);
+
+    esp_err_t ret=spi_device_transmit(*(handler), transaction);
+
+    if(ret == ESP_OK)
+    {
+	//Convert from four element byte array to 32 bit integer variable with endianness preserved
+	for(int i=0; i<=3; i++)
+	{
+            readstate += (transaction->rx_data[i] << (i*8)); //Pointer points to transaction rx array
+	}
+        ESP_LOGI(TAG, "Note: SPI read success");
+	ESP_LOGI(TAG, "Value read is %d", readstate);
     }
-    return ESP_OK;
-}
-
-/* Initialize Wi-Fi as sta and set scan method */
-static void wifi_scan(void)
-{
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = DEFAULT_SSID,
-            .password = DEFAULT_PWD,
-            .scan_method = DEFAULT_SCAN_METHOD,
-            .sort_method = DEFAULT_SORT_METHOD,
-            .threshold.rssi = DEFAULT_RSSI,
-            .threshold.authmode = DEFAULT_AUTHMODE,
-        },
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-}
-
-int getStrength(int points){
-
-   wifi_ap_record_t wifidata;
-   long rssi = 0;
-    long averageRSSI=0;
-
-    for (int i=0;i < points;i++){
-       if (esp_wifi_sta_get_ap_info(&wifidata)==0){
-
-       rssi += wifidata.rssi;
-       vTaskDelay(10 / portTICK_PERIOD_MS);
-       }
+    else
+    {
+        ESP_LOGE(TAG, "ERROR: SPI read failure");
     }
-
-   averageRSSI=rssi/points;
-   return averageRSSI;
+    
 }
-
-esp_err_t pingResults(ping_target_id_t msgType, esp_ping_found * pf){
-   printf("AvgTime:%.1fmS Sent:%d Rec:%d Err:%d min(mS):%d max(mS):%d ", (float)pf->total_time/pf->recv_count, pf->send_count, pf->recv_count, pf->err_count, pf->min_time, pf->max_time );
-   printf("Resp(mS):%d Timeouts:%d Total Time:%d\n",pf->resp_time, pf->timeout_count, pf->total_time);
-   waiting_results = 0;
-   return ESP_OK;
-}
-
 
 void app_main()
 {
-    printf("esp32 Latency and Packdrop test!\n");
+    esp_err_t ret;
+    spi_device_handle_t spi;
+    //Configure the spi bus
+    spi_bus_config_t buscfg={
+        .miso_io_num=PIN_NUM_MISO,
+        .mosi_io_num=PIN_NUM_MOSI,
+        .sclk_io_num=PIN_NUM_CLK,
+        .quadwp_io_num=-1,
+        .quadhd_io_num=-1
+    };
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+    spi_device_interface_config_t devcfg={
+        .clock_speed_hz=SCLK_SPEED,             //Clock out at SCLK_SPEED Hz
+        .address_bits=0,			//No address bits in transaction
+	.command_bits=0,			//No command bits in transaction
+        .mode=0,                                //SPI mode 0(CPOL=0,CPHA=0)
+        .spics_io_num=PIN_NUM_CS,               //CS pin
+        .queue_size=7,                          //We want to be able to queue 7 transactions at a time
+    };
+    //Initialize the SPI bus
+    ret=spi_bus_initialize(HSPI_HOST, &buscfg, 1);
+    assert(ret==ESP_OK);
+    //Attach the LCD to the SPI bus
+    ret=spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
+    assert(ret==ESP_OK);
 
-    printf("silicon revision %d, ", chip_info.revision);
+    spi_transaction_t tx;
+    memset(&tx, 0, sizeof(tx));
+    //Store transmit and receive data in the transaction struct itself as both are 4 bytes and less.
+    tx.flags=SPI_TRANS_USE_TXDATA|SPI_TRANS_USE_RXDATA;
+    tx.length=16;
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-
-    // Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
+    while(1)
+    {
+        set_read_basys3_leds(&tx, &spi);
+	vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-    ESP_ERROR_CHECK( ret );
 
-    wifi_scan();
-
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
-    while(TRUE) {
-       vTaskDelay(1000 / portTICK_PERIOD_MS);
-       if ((gotConn)&&(!waiting_results)) {
-           esp_ping_set_target(PING_TARGET_IP_ADDRESS_COUNT, &ping_count, sizeof(uint32_t));
-              esp_ping_set_target(PING_TARGET_RCV_TIMEO, &ping_timeout, sizeof(uint32_t));
-              esp_ping_set_target(PING_TARGET_DELAY_TIME, &ping_delay, sizeof(uint32_t));
-           esp_ping_set_target(PING_TARGET_IP_ADDRESS, &device.addr, sizeof(uint32_t));
-           esp_ping_set_target(PING_TARGET_RES_FN, &pingResults, sizeof(pingResults));
-
-           printf("\nPinging Gateway IP %s WiFi rssi %d\n",addr,getStrength(10));
-           if (first){
-           first = FALSE;
-           }
-           printf("%s, %d, ", addr, getStrength(10));
-           ping_init();
-           waiting_results = 1;
-        }
-
-    }
 }
