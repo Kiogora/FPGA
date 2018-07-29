54af3a5b0a1e5ff010e2837d3febbb092eb86716
Alois Mbutura <aloismbutura@gmail.com>
2018-07-29 23:35:10 +0300

    Update VHDL design
    1. Went with a more common template to code the SPI slave FSM.
    2. Involved use of a syncronising section(asynchronous inputs to imitate a FF)
    3. Also involved use of a combinatorial section that took care of the present state using signals.
    4. The output(miso/mosi) did not have an effect on the two above behaviorally thus were in no sensitivity list.
    5. The form above to code the FSM was well documented in the book Free range VHDL.
    6. It should be noted that the design for this would not involve full duplex read/write but just simplex write for simplicity.
    7. The next design should have full duplex read and write that builds on top of this.

--- Basys_3/fpga_spi_slave_simple/fpga/spi_slave_simple.srcs/sources_1/new/spi_slave_simple.vhd
+++ Basys_3/fpga_spi_slave_simple/fpga/spi_slave_simple.srcs/sources_1/new/spi_slave_simple.vhd
@@ -37,62 +37,57 @@ entity spi_slave_simple is
     generic(datawidth: integer:= 16);
     port ( sck: in std_logic;
            ss: in std_logic;
-           mosi: in std_logic;
+           --mosi: in std_logic;
            miso: out std_logic;
-           LED: inout std_logic_vector(datawidth-1 downto 0) := (others => '0'));
+           data: in std_logic_vector(datawidth-1 downto 0));
            --ready: out std_logic);
 end spi_slave_simple;
 
 architecture behavioral of spi_slave_simple is
     -- Internal entity signals
-    signal data_temp_in : std_logic_vector(datawidth-1 downto 0) := (others => '0');
-    signal data_temp_out : std_logic_vector(datawidth-1 downto 0):= (others => '0');
+    type state_type is (idle, load, shift);
+    signal present_state, next_state : state_type;
+    
+    --signal input_shift_register : std_logic_vector(datawidth-1 downto 0);
+    signal output_shift_register : std_logic_vector(datawidth-1 downto 0);
+
+    --signal index : integer;
     
 begin
 
---Operations at start and end of transaction.
-    process (ss)
-    begin
-        if rising_edge(ss) then
-            --Write data received from mosi in buffer of transaction width to the LEDs
-            --The data should be read out dependent on the ready port by external entity
-            LED <= data_temp_in;
-            --Signal that data in buffer is ok to  read out.
-            --ready <= '1';
-        end if;
-    end process;
-    
-    process (ss)
-    begin
-        --Start of spi transaction
-        if falling_edge(ss) then
-            --Signal that data in buffer is in flux. Should not be trusted
-            --ready <= '0';
-            --Read data from LEDs into buffer of the transaction width to be written to mosi at each sck rising edge
-            data_temp_out <= LED;
-        end if;
-    end process;
-    
---Operations within the transaction itself.
-    process (sck)
-        begin
-        if rising_edge(sck) then
-            if ss = '0' then
-                --As long as there is a rising edge on sck when ss is active low, always left shift the data in buffer. Even when in error
-                --Write to miso and read from mosi concurrently
-                --A transaction on the spi bus occurs by default as msb first thus we unpack msb to lsb on mosi and miso.
-                --Left shift [length-1:1]<-[length-2:0] then write to index 0 with value from mosi
-                --This means that we do nothave to keep track of an index variable, just the buffer length
-                data_temp_in(datawidth-1 downto 0) <= data_temp_in(datawidth-2 downto 0) & mosi;           
-                --Write msb to the spi bus and left shift the buffer data while adding zeros to the buffer lsb at each sck rising edge 
-                miso <= data_temp_out(datawidth-1);
-                data_temp_out(datawidth-1 downto 0) <= data_temp_out(datawidth-2 downto 0) & '0';
---            else
---                --Set the mosi into Hi-Z if this slave is not selected. Overkill for this example as there is only one slave on SPI bus.
---                mosi<=(others => 'z');
-            end if;
-        end if;
-    end process;
-    
+--Realises to a FF
+--Has factors affecting state transition. Usually asynchronous.
+state_sync: process(sck, ss)
+begin
+    --Force next state as idle state when ss is high, regardless of sck
+    if(ss = '1') then 
+        present_state <= idle;
+    elsif(rising_edge(sck)) then 
+        present_state <= next_state;
+    end if;
+end process state_sync;
+
+--Outputs are function of past inputs and outputs, no present input on any data lines
+combinatorial : process(present_state)
+begin
+case present_state is
+when idle =>
+    output_shift_register<=(others=>'0');
+    --input_shift_register<=(others=>'0');
+    --index <= datawidth-1;
+    next_state <= load;
+when load =>
+    output_shift_register<=data;
+    next_state <= shift;
+when shift =>
+    miso<=output_shift_register(datawidth-1);
+    output_shift_register(datawidth-1 downto 0)<=output_shift_register(datawidth-2 downto 0) & '0';
+    next_state <= shift;
+--Catch all to avoid latch inferencing by synthesis tools
+when others =>
+next_state <= idle;
+
+end case;
+end process combinatorial;
 
 end behavioral;
